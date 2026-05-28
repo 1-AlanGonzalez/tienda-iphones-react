25bba11f5ae46b538e8dcb0027f99a67d2ed62f1
Alan Gonzalez <alanagustin724@gmail.com>
2026-05-28 15:42:25 -0300

    comienzo_page_producto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Productos from "./pages/Productos";
+import "./App.css";
 
 function App() {
   return (
-   <NavBar />    
+   <BrowserRouter>
+        <NavBar />
+        
+        <Routes>
+          <Route path="/productos" element={<Productos />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
