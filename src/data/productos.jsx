a8d5a67b75af8fd5bad6a28743ea116b4523a417
Alan Gonzalez <alanagustin724@gmail.com>
2026-06-05 15:35:00 -0300

    productos en proceso

--- src/data/productos.jsx
+++ src/data/productos.jsx
@@ -1,22 +1,33 @@
 // IMPORTO LAS IMAGENES
-import iphone17_orange from "../assets/images/iphone-17-pro-max-orange.jpg";
-import iphone17_white from "../assets/images/iphone-17-pro-max-white.jpg";
+
+// IPHONE 16
+import iphone16_negro from "../assets/images/iphone-16-negro.png";
+import iphone16_blanco from "../assets/images/iphone-16-blanco.png";
+import iphone16_celeste from "../assets/images/iphone-16-celeste.png";
+
+// IPHONE 17
 import iphone17_black from "../assets/images/iphone-17-negro.png";
 import iphone17_blanco from "../assets/images/iphone-17-blanco.png";
-import iphone17_green from "../assets/images/IPhone-17-verde.png"
-import iphone17_sky_blue from "../assets/images/Iphone-17-celeste.png"
+import iphone17_green from "../assets/images/iphone-17-verde.png"
+import iphone17_sky_blue from "../assets/images/iphone-17-celeste.png"
+import iphone17_pro_blanco from "../assets/images/iphone-17-pro-blanco.png"
+import iphone17_pro_azul from "../assets/images/iphone-17-pro-azul.png"
+import iphone17_pro_naranja from "../assets/images/iphone-17-pro-naranja.png"
+import iphone17_promax_blanco from "../assets/images/iphone-17-promax-blanco.png"
+import iphone17_promax_azul from "../assets/images/iphone-17-promax-negro.png"
+import iphone17_promax_naranja from "../assets/images/iphone-17-promax-naranja.png"
 
 export const productos = [
   {
     id: 1,
     nombre: "iPhone 17",
     linea: "Standard",
-    color: "Black",
+    color: "Negro",
     categoria: "iPhone",
     almacenamiento: "256GB",
     precio: 799,
     imagen: iphone17_black,
-    descripcion: "iPhone 17 con chip A19, pantalla Super Retina XDR de 6.3 pulgadas con ProMotion de 120Hz y sistema de cámaras de 48MP.",
+    descripcion: "iPhone 17 con chip A19, pantalla Super Retina XDR de 6.3 pulgadas, sistema de cámaras de 48MP y gran autonomía.",
     stock: 10,
     tag: "Nuevo"
   },
@@ -30,7 +41,7 @@ export const productos = [
     almacenamiento: "256GB",
     precio: 799,
     imagen: iphone17_blanco,
-    descripcion: "iPhone 17 con chip A19, pantalla Super Retina XDR de 6.3 pulgadas con ProMotion de 120Hz y sistema de cámaras de 48MP.",
+    descripcion: "iPhone 17 con chip A19, pantalla Super Retina XDR de 6.3 pulgadas, sistema de cámaras de 48MP y gran autonomía.",
     stock: 10,
     tag: "Nuevo"
   },
@@ -44,7 +55,7 @@ export const productos = [
     almacenamiento: "256GB",
     precio: 799,
     imagen: iphone17_green,
-    descripcion: "iPhone 17 con chip A19, pantalla Super Retina XDR de 6.3 pulgadas con ProMotion de 120Hz y sistema de cámaras de 48MP.",
+    descripcion: "iPhone 17 con chip A19, pantalla Super Retina XDR de 6.3 pulgadas, sistema de cámaras de 48MP y gran autonomía.",
     stock: 10,
     tag: "Nuevo"
   },
@@ -58,48 +69,174 @@ export const productos = [
     almacenamiento: "256GB",
     precio: 799,
     imagen: iphone17_sky_blue,
-    descripcion: "iPhone 17 con chip A19, pantalla Super Retina XDR de 6.3 pulgadas con ProMotion de 120Hz y sistema de cámaras de 48MP.",
+    descripcion: "iPhone 17 con chip A19, pantalla Super Retina XDR de 6.3 pulgadas, sistema de cámaras de 48MP y gran autonomía.",
     stock: 10,
     tag: "Nuevo"
   },
 
   {
     id: 5,
+    nombre: "iPhone 17 Pro",
+    linea: "Pro",
+    color: "Blanco",
+    categoria: "iPhone",
+    almacenamiento: "256GB",
+    precio: 1099,
+    imagen: iphone17_pro_blanco,
+    descripcion: "iPhone 17 Pro con chip A19 Pro, pantalla Super Retina XDR de 6.3 pulgadas con ProMotion de 120Hz, sistema de cámaras profesionales de 48MP y acabado en titanio.",
+    stock: 10,
+    tag: "Nuevo"
+  },
+
+  {
+    id: 6,
+    nombre: "iPhone 17 Pro",
+    linea: "Pro",
+    color: "Azul",
+    categoria: "iPhone",
+    almacenamiento: "256GB",
+    precio: 1099,
+    imagen: iphone17_pro_azul,
+    descripcion: "iPhone 17 Pro con chip A19 Pro, pantalla Super Retina XDR de 6.3 pulgadas con ProMotion de 120Hz, sistema de cámaras profesionales de 48MP y acabado en titanio.",
+    stock: 10,
+    tag: "Nuevo"
+  },
+
+  {
+    id: 7,
+    nombre: "iPhone 17 Pro",
+    linea: "Pro",
+    color: "Naranja",
+    categoria: "iPhone",
+    almacenamiento: "256GB",
+    precio: 1099,
+    imagen: iphone17_pro_naranja,
+    descripcion: "iPhone 17 Pro con chip A19 Pro, pantalla Super Retina XDR de 6.3 pulgadas con ProMotion de 120Hz, sistema de cámaras profesionales de 48MP y acabado en titanio.",
+    stock: 10,
+    tag: "Nuevo"
+  },
+
+  {
+    id: 8,
     nombre: "iPhone 17 Pro Max",
     linea: "Pro Max",
     color: "Naranja",
     categoria: "iPhone",
-    almacenamiento: "512GB",
-    precio: 1399,
-    imagen: iphone17_orange,
-    descripcion: "El iPhone más avanzado. Pantalla Super Retina XDR 6.9\", chip A19 Pro, cámara 48MP y batería de hasta 39 horas.",
+    almacenamiento: "256GB",
+    precio: 1299,
+    imagen: iphone17_promax_blanco,
+    descripcion: "iPhone 17 Pro Max con chip A19 Pro, pantalla Super Retina XDR de 6.9 pulgadas con ProMotion de 120Hz, sistema de cámaras profesionales de 48MP, batería de larga duración y acabado en titanio.",
     stock: 10,
     tag: "Nuevo"
-  },
-  {
-    id: 6,
+},
+
+{
+    id: 9,
     nombre: "iPhone 17 Pro Max",
     linea: "Pro Max",
-    color: "Blanco",
+    color: "Azul",
     categoria: "iPhone",
     almacenamiento: "256GB",
-    precio: 799,
-    imagen: iphone17_white,
-    descripcion: "iPhone 17 Pro Max con chip A19, pantalla Super Retina XDR de 6.3 pulgadas con ProMotion de 120Hz y sistema de cámaras de 48MP.",
-    stock: 15,
+    precio: 1299,
+    imagen: iphone17_promax_azul,
+    descripcion: "iPhone 17 Pro Max con chip A19 Pro, pantalla Super Retina XDR de 6.9 pulgadas con ProMotion de 120Hz, sistema de cámaras profesionales de 48MP, batería de larga duración y acabado en titanio.",
+    stock: 10,
     tag: "Nuevo"
-  },
+},
+
+{
+    id: 10,
+    nombre: "iPhone 17 Pro Max",
+    linea: "Pro Max",
+    color: "Naranja",
+    categoria: "iPhone",
+    almacenamiento: "256GB",
+    precio: 1299,
+    imagen: iphone17_promax_naranja,
+    descripcion: "iPhone 17 Pro Max con chip A19 Pro, pantalla Super Retina XDR de 6.9 pulgadas con ProMotion de 120Hz, sistema de cámaras profesionales de 48MP, batería de larga duración y acabado en titanio.",
+    stock: 10,
+    tag: "Nuevo"
+},
+
+{
+    id: 11,
+    nombre: "iPhone 16",
+    linea: "Standard",
+    color: "Negro",
+    categoria: "iPhone",
+    almacenamiento: "128GB",
+    precio: 799,
+    imagen: iphone16_negro,
+    descripcion: "iPhone 16 con chip A18, pantalla Super Retina XDR de 6.1 pulgadas, cámara principal de 48MP y excelente rendimiento para el uso diario.",
+    stock: 10,
+    tag: "Oferta"
+},
+
+{
+    id: 12,
+    nombre: "iPhone 16",
+    linea: "Standard",
+    color: "Blanco",
+    categoria: "iPhone",
+    almacenamiento: "128GB",
+    precio: 799,
+    imagen: iphone16_blanco,
+    descripcion: "iPhone 16 con chip A18, pantalla Super Retina XDR de 6.1 pulgadas, cámara principal de 48MP y excelente rendimiento para el uso diario.",
+    stock: 10,
+    tag: "Oferta"
+},
+
+{
+    id: 13,
+    nombre: "iPhone 16",
+    linea: "Standard",
+    color: "Celeste",
+    categoria: "iPhone",
+    almacenamiento: "128GB",
+    precio: 799,
+    imagen: iphone16_celeste,
+    descripcion: "iPhone 16 con chip A18, pantalla Super Retina XDR de 6.1 pulgadas, cámara principal de 48MP y excelente rendimiento para el uso diario.",
+    stock: 10,
+    tag: "Oferta"
+},
+
+  // {
+  //   id: 5,
+  //   nombre: "iPhone 17 Pro Max",
+  //   linea: "Pro Max",
+  //   color: "Naranja",
+  //   categoria: "iPhone",
+  //   almacenamiento: "512GB",
+  //   precio: 1399,
+  //   imagen: iphone17_orange,
+  //   descripcion: "El iPhone más avanzado. Pantalla Super Retina XDR 6.9\", chip A19 Pro, cámara 48MP y batería de hasta 39 horas.",
+  //   stock: 10,
+  //   tag: "Nuevo"
+  // },
+  // {
+  //   id: 6,
+  //   nombre: "iPhone 17 Pro Max",
+  //   linea: "Pro Max",
+  //   color: "Blanco",
+  //   categoria: "iPhone",
+  //   almacenamiento: "256GB",
+  //   precio: 799,
+  //   imagen: iphone17_white,
+  //   descripcion: "iPhone 17 Pro Max con chip A19, pantalla Super Retina XDR de 6.3 pulgadas con ProMotion de 120Hz y sistema de cámaras de 48MP.",
+  //   stock: 15,
+  //   tag: "Nuevo"
+  // },
 
   
-  {
-    id: 7,
-    nombre: "AirPods Pro 2",
-    linea: "AirPods Pro",
-    categoria: "AirPods",
-    precio: 449,
-    imagen: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MQD83?wid=400&hei=400&fmt=png-alpha",
-    descripcion: "Cancelación activa de ruido adaptativa, audio espacial personalizado y hasta 30 horas de batería.",
-    stock: 30,
-    tag: "Oferta"
-  }
+  // {
+  //   id: 7,
+  //   nombre: "AirPods Pro 2",
+  //   linea: "AirPods Pro",
+  //   categoria: "AirPods",
+  //   precio: 449,
+  //   imagen: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MQD83?wid=400&hei=400&fmt=png-alpha",
+  //   descripcion: "Cancelación activa de ruido adaptativa, audio espacial personalizado y hasta 30 horas de batería.",
+  //   stock: 30,
+  //   tag: "Oferta"
+  // }
 ];
